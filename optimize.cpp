4fa5464f461f541f6aca70c614c6e15ba1d6277d
w <wvvwvvvvwvvw@gmail.com>
2022-03-26 04:58:27 -0700

    add remake

--- optimize.cpp
+++ optimize.cpp
@@ -1,492 +1,222 @@
 #include <algorithm>
 #include <cmath>
-#include <cstring>
-#include <fstream>
+#include <cstdlib>
 #include <iostream>
 #include <math.h>
+#include <sstream>
+#include <string.h>
 #include <vector>
 
+// sampling resolution
+#define RES 10
+
 struct Point {
-  double x, y;
-  double dx, dy;
+  double x;
+  double y;
+
+  Point operator+(const Point &a) { return {x + a.x, y + a.y}; }
+  Point operator-(const Point &a) { return {x - a.x, y - a.y}; }
+  Point operator+(const double &a) { return {x + a, y + a}; }
+  Point operator-(const double &a) { return {x - a, y - a}; }
+  Point operator*(const double &a) { return {x * a, y * a}; }
 };
 
-struct IT {
-  int i;
-  double t;
-};
-
-struct ITT {
-  int i;
-  double t;
-  double t2;
-};
-
-struct ITPoint {
-  int i;
-  double t;
-  Point p;
-};
-
-double dist(Point a, Point b) {
-  return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
+double dist(Point p0, Point p1) {
+  return sqrt(pow(p1.x - p0.x, 2) + pow(p1.y - p1.x, 2));
 }
 
-bool sim(Point a, Point b) {
-  return round(a.x) == round(b.x) && round(a.y) == round(b.y);
+double shoelace(Point p0, Point p1, Point p2, Point p3) {
+  return abs(((p0.x * p1.y - p0.y * p1.x) + (p1.x * p2.y - p1.y * p2.x) +
+              (p2.x * p3.y - p2.y * p3.x) + (p3.x * p0.y - p3.y * p0.x)) /
+             2);
 }
 
-double length(Point p) { return sqrt(pow(p.x, 2) + pow(p.y, 2)); }
-
-double cubic(double a, double b, double c, double d, double x) {
-  return a * pow(x, 3) + b * pow(x, 2) + c * x + d;
-}
-
-Point catmull_rom(Point p0, Point p1, Point p2, Point p3, double t) {
+Point catmull_rom(Point points[4], double t) {
   double alpha = 0.5;
 
-  double a1, b1, c1, d1, a2, b2, c2, d2;
+  Point p0 = points[0];
+  Point p1 = points[1];
+  Point p2 = points[2];
+  Point p3 = points[3];
 
-  a1 = -p0.x + 3 * p1.x - 3 * p2.x + p3.x;
-  a2 = -p0.y + 3 * p1.y - 3 * p2.y + p3.y;
+  Point a = p1 * 3.0 - p2 * 3.0 + p3 - p0;
+  Point b = p0 * 2.0 - p1 * 5.0 + p2 * 4.0 - p3;
+  Point c = p2 - p0;
+  Point d = p1 * 2.0;
 
-  b1 = 2 * p0.x - 5 * p1.x + 4 * p2.x - p3.x;
-  b2 = 2 * p0.y - 5 * p1.y + 4 * p2.y - p3.y;
-
-  c1 = -p0.x + p2.x;
-  c2 = -p0.y + p2.y;
-
-  d1 = 2 * p1.x;
-  d2 = 2 * p1.y;
-
-  double t3 = pow(t, 3);
-  double t2 = pow(t, 2);
-
-  Point p = {(a1 * t3 + b1 * t2 + c1 * t + d1) * alpha,
-             (a2 * t3 + b2 * t2 + c2 * t + d2) * alpha,
-             (3 * t2 * a1 + 2 * t * b1 + c1) * alpha,
-             (3 * t2 * a2 + 2 * t * b2 + c2) * alpha};
-
-  return p;
+  return (a * pow(t, 3) + b * pow(t, 2) + c * t + d) * alpha;
 }
 
-Point catmull_rom(const std::vector<Point> points, int start, double t) {
-  Point p0, p1, p2, p3;
-  p0 = start ? points[start - 1] : points[0];
-  p1 = points[start + 0];
-  p2 = points[start + 1];
-  p3 = start < points.size() - 2 ? points[start + 2] : p2;
+template <typename T>
+double error_remove(std::vector<Point> &points, int remove, T &remove_op) {
+  int start = 0;
+  int center = 0;
+  int end = 0;
 
-  return catmull_rom(p0, p1, p2, p3, t);
-}
+  std::vector<Point> line1;
+  std::vector<Point> line2;
 
-double cubic_solve(double a, double b, double c, double d, Point p0, Point p1,
-                   Point p2, Point p3, Point target) {
-  if (a == 0) {
-    return 0;
-  }
+  for (int i = remove - 3; i <= remove + 3; i++) {
+    if (i >= 0 && i < points.size()) {
+      if (i == remove - 3 && line1.size() == 0) {
+        start = 1;
+      }
 
-  if (d == 0) {
-    return 0;
-  }
+      if (i == remove) {
+        center = line1.size();
+      }
 
-  b /= a;
-  c /= a;
-  d /= a;
+      line1.push_back(points[i]);
+      line2.push_back(points[i]);
 
-  double disc, q, r, dum1, s, t, term1, r13;
-  q = (3.0 * c - (b * b)) / 9.0;
-  r = -(27.0 * d) + b * (9.0 * c - 2.0 * (b * b));
-  r /= 54.0;
-  disc = q * q * q + r * r;
-  term1 = b / 3.0;
-
-  // One real root, two are complex.
-  if (disc > 0) {
-    s = r + sqrt(disc);
-    s = (s < 0) ? -pow(-s, (1.0 / 3.0)) : pow(s, (1.0 / 3.0));
-    t = r - sqrt(disc);
-    t = (t < 0) ? -pow(-t, (1.0 / 3.0)) : pow(t, (1.0 / 3.0));
-    double root1 = -term1 + s + t;
-    term1 += (s + t) / 2.0;
-    double real = term1;
-
-    double y1 = dist(catmull_rom(p0, p1, p2, p3, root1), target);
-    double y2 = dist(catmull_rom(p0, p1, p2, p3, real), target);
-    if (y1 < y2) {
-      return root1;
-    } else {
-      return real;
+      if (i == remove + 2) {
+        end = line1.size() - 1;
+      }
     }
   }
 
-  // All roots real, at least two are equal.
-  if (disc == 0) {
-    r13 = (r < 0) ? -pow(-r, (1.0 / 3.0)) : pow(r, (1.0 / 3.0));
-    double root1 = -term1 + 2.0 * r13;
-    double root2 = -(r13 + term1);
-    double y1 = dist(catmull_rom(p0, p1, p2, p3, root1), target);
-    double y2 = dist(catmull_rom(p0, p1, p2, p3, root2), target);
-    if (y1 < y2) {
-      return root1;
-    } else {
-      return root2;
+  remove_op(line1, line2, start, center, end);
+
+  if (end == 0) {
+    end = line1.size() - 1;
+  }
+
+  std::vector<Point> line1_spls;
+  std::vector<Point> line2_spls;
+
+  auto add_samples = [](std::vector<Point> &out_points,
+                        std::vector<Point> points, int i, int count) {
+    out_points.push_back(points[i]);
+
+    Point line[4] = {
+        i ? points[i - 1] : points[0],
+        points[i],
+        points[i + 1],
+        i + 2 < points.size() ? points[i + 2] : points[i + 1],
+    };
+
+    for (int j = 1; j < count; j++) {
+      Point p = catmull_rom(line, (double)j / count);
+      out_points.push_back(p);
     }
+  };
+
+  for (int i = start; i < end; i++) {
+    add_samples(line1_spls, line1, i, RES);
+  }
+  line1_spls.push_back(line1[end]);
+
+  for (int i = start; i < end - 1; i++) {
+    add_samples(line2_spls, line2, i, i == center - 1 ? RES * 2 : RES);
+  }
+  line2_spls.push_back(line2[end - 1]);
+
+  Point last_p1 = line1_spls[0];
+  Point last_p2 = line2_spls[0];
+
+  double total_area = 0;
+  for (int i = 1; i < line1_spls.size(); i++) {
+    total_area += shoelace(last_p1, last_p2, line2_spls[i], line1_spls[i]);
+    last_p1 = line1_spls[i];
+    last_p2 = line2_spls[i];
   }
 
-  // All roots real
-  q = -q;
-  dum1 = q * q * q;
-  dum1 = acos(r / sqrt(dum1));
-  r13 = 2.0 * sqrt(q);
-
-  double root1 = -term1 + r13 * cos(dum1 / 3.0);
-  double root2 = -term1 + r13 * cos((dum1 + 2.0 * M_PI) / 3.0);
-  double root3 = -term1 + r13 * cos((dum1 + 4.0 * M_PI) / 3.0);
-
-  double y1 = dist(catmull_rom(p0, p1, p2, p3, root1), target);
-  double y2 = dist(catmull_rom(p0, p1, p2, p3, root2), target);
-  double y3 = dist(catmull_rom(p0, p1, p2, p3, root3), target);
-
-  if (y1 < y2 && y1 < y3) {
-    return root1;
-  } else if (y2 < y3) {
-    return root2;
-  } else {
-    return root3;
-  }
+  return total_area;
 }
 
-ITPoint catmull_rom_solve(const std::vector<Point> &points, int start,
-                          Point target) {
-  double alpha = 0.5;
-  Point p0, p1, p2, p3;
-  double a1, b1, c1, d1, a2, b2, c2, d2;
-
-  p0 = start > 0 ? points[start - 1] : points[0];
-  p1 = points[start + 0];
-  p2 = points[start + 1];
-  p3 = start < points.size() - 2 ? points[start + 2] : p2;
-
-  a1 = -p0.x + 3 * p1.x - 3 * p2.x + p3.x;
-  b1 = 2 * p0.x - 5 * p1.x + 4 * p2.x - p3.x;
-  c1 = -p0.x + p2.x;
-  d1 = 2 * p1.x;
-
-  a2 = -p0.y + 3 * p1.y - 3 * p2.y + p3.y;
-  b2 = 2 * p0.y - 5 * p1.y + 4 * p2.y - p3.y;
-  c2 = -p0.y + p2.y;
-  d2 = 2 * p1.y;
-
-  double root_x =
-      cubic_solve(a1, b1, c1, d1 - target.x / alpha, p0, p1, p2, p3, target);
-  double root_y =
-      cubic_solve(a2, b2, c2, d2 - target.y / alpha, p0, p1, p2, p3, target);
-  Point p_x = catmull_rom(points, start, root_x);
-  Point p_y = catmull_rom(points, start, root_y);
-
-  if (dist(p_x, target) < dist(p_y, target)) {
-    return {start, root_x, p_x};
-  } else {
-    return {start, root_y, p_y};
-  }
-}
-
-// find t
-Point find_on_spline(const std::vector<Point> &points, Point point, int start) {
-  std::vector<ITPoint> candidate_t;
-
-  int first = std::max(0, start - 4);
-  int last = std::min((int)points.size() - 1, start + 4);
-
-  for (int i = first; i < last; i++) {
-    candidate_t.push_back(catmull_rom_solve(points, i, point));
-  }
-
-  ITPoint closest = candidate_t[0];
-  double closest_distance = dist(point, closest.p);
-  int closest_index = 0;
-
-  for (int i = 1; i < candidate_t.size(); i++) {
-    double candidate_distance = dist(point, candidate_t[i].p);
-    if (candidate_distance < closest_distance) {
-      closest_distance = candidate_distance;
-      closest_index = i;
-      closest = candidate_t[i];
-    }
-  }
-
-  return closest.p;
-}
-
-std::vector<Point> copy_points(const std::vector<Point> &points) {
-  std::vector<Point> new_points(points.size());
-  memcpy(new_points.data(), points.data(), sizeof(Point) * points.size());
-  return new_points;
-}
-
-double polygonArea(double X[], double Y[], int n) {
-  // Initialize area
-  double area = 0.0;
-
-  // Calculate value of shoelace formula
-  int j = n - 1;
-  for (int i = 0; i < n; i++) {
-    area += (X[j] + X[i]) * (Y[j] - Y[i]);
-    j = i; // j is previous vertex to i
-  }
-
-  // Return absolute value
-  return abs(area / 2.0);
-}
-
-double calculate_error(const std::vector<Point> samples,
-                       const std::vector<Point> &points, int start) {
-
-  std::vector<Point> samples_points;
-  for (Point point : samples) {
-    samples_points.push_back(find_on_spline(points, point, start));
-  }
-
-  double area = 0;
-  for (int i = 0; i < samples.size() - 1; i++) {
-    Point p0 = samples[i];
-    Point p1 = samples[i + 1];
-
-    Point p2 = samples_points[i];
-    Point p3 = samples_points[i + 1];
-
-    double x[] = {p0.x, p1.x, p3.x, p2.x};
-    double y[] = {p0.y, p1.y, p3.y, p2.y};
-
-    area += polygonArea(x, y, 4);
-  }
-
-  return area;
-}
-
-double arc_length(const std::vector<Point> &points, int start, double res = 1) {
-  double l = 0;
-  Point last_point = catmull_rom(points, start, 0);
-  for (double t = res / length({last_point.dx, last_point.dy}); t < 1;) {
-    Point p = catmull_rom(points, start, t);
-    l += dist(last_point, p);
-    last_point = p;
-    t += res / length(p);
-  }
-  l += dist(catmull_rom(points, start, 1), last_point);
-  return l;
-}
-
-std::vector<double> equidistant(const std::vector<Point> &points, int start,
-                                double res) {
-  std::vector<double> sample_t;
-  Point last_point = catmull_rom(points, start, 0);
-  for (double t = res / length({last_point.dx, last_point.dy}); t < 1;) {
-    Point p = catmull_rom(points, start, t);
-    last_point = p;
-    sample_t.push_back(t);
-    t += res / length(p);
-  }
-  return sample_t;
-}
-
-// A - B - (C) - D - E
-std::vector<Point> get_samples(std::vector<Point> points, int start,
-                               double res = 1) {
-  std::vector<Point> samples;
-
-  // [A - B)
-  if (start > 1) {
-    samples.push_back(points[start - 2]);
-    for (double t : equidistant(points, start - 2, res)) {
-      samples.push_back(catmull_rom(points, start - 2, t));
-    }
-  }
-  // [B - C)
-  samples.push_back(points[start - 1]);
-  for (double t : equidistant(points, start - 1, res)) {
-    samples.push_back(catmull_rom(points, start - 1, t));
-  }
-  // [C - D]
-  samples.push_back(points[start]);
-  for (double t : equidistant(points, start, res)) {
-    samples.push_back(catmull_rom(points, start, t));
-  }
-  samples.push_back(points[start + 1]);
-  // (D - E]
-  if (start < points.size() - 2) {
-    for (double t : equidistant(points, start + 1, res)) {
-      samples.push_back(catmull_rom(points, start + 1, t));
-    }
-    samples.push_back(points[start + 2]);
-  }
-
-  return samples;
-}
-
-IT optimize(std::vector<IT> &errors, std::vector<Point> &points, int start = 1,
-            int end = -1, double move = 0, int recalculate = 0) {
-  for (int i = start; i < points.size() + end; i++) {
-    if (recalculate == -1 || recalculate != -2 && abs(i - recalculate) > 5) {
-      continue;
-    }
-
-    double d = arc_length(points, i - 1) + arc_length(points, i);
-    if (i > 1) {
-      d += arc_length(points, i - 2);
-    }
-    if (i < points.size() - 2) {
-      d += arc_length(points, i + 1);
-    }
-
-    std::vector<Point> new_points = copy_points(points);
-    new_points.erase(new_points.begin() + i);
-
-    if (i < points.size() + end - 1) {
-      new_points[i] = catmull_rom(points, i, move);
-    }
-
-    std::vector<Point> samples = get_samples(points, i);
-    double error = calculate_error(samples, new_points, i);
-    errors[i] = {i, error};
-    // if (error < d * 0.1) {
-    //   points = new_points;
-    //   i++;
-    // }
-  }
-
-  IT smallest = errors[start];
-  int in = start;
-
-  for (int i = start + 1; i < points.size() + end; i++) {
-    if (errors[i].t < smallest.t) {
-      in = i;
-      smallest = errors[i];
-    }
-  }
-
-  return smallest;
-
-  return {};
-}
-
-void step(std::vector<Point> &points, int direction) {
-  if (direction > 0) {
-    Point p1 = catmull_rom(points, 0, 0);
-    Point p2 = {p1.x + p1.dx, p1.y + p1.dy};
-    p2 = find_on_spline(points, p2, 0);
-
-    double d = dist(p1, p2);
-
-    if (d < 2) {
-      points.insert(points.begin() + 1, p2);
-    }
-  } else {
-    Point p1 = catmull_rom(points, points.size() - 2, 1);
-    Point p2 = {p1.x - p1.dx, p1.y - p1.dy};
-    p2 = find_on_spline(points, p2, points.size() - 2);
-
-    double d = dist(p1, p2);
-
-    if (d < 2) {
-      points.insert(points.begin() + points.size() - 1, p2);
-    }
-  }
-}
-
-int main() {
-  std::ifstream in("original_spline");
+int main(int argc, char *argv[]) {
+  double error = atof(argv[1]);
 
   std::vector<Point> points;
 
   double x, y;
-  while (in >> x >> y) {
+  std::string tok;
+  for (std::string line; std::getline(std::cin, line);) {
+    std::istringstream stream(line);
+    stream >> tok;
+    x = stof(tok, nullptr);
+    stream >> tok;
+    y = stof(tok, nullptr);
     points.push_back({x, y});
   }
 
-  size_t starting_size = points.size();
-  size_t current_size = starting_size;
-  printf("%zu points\n", starting_size);
+  { // delete middle
+    auto remove_op = [](std::vector<Point> &line1, std::vector<Point> &line2,
+                        int start, int center,
+                        int end) { line2.erase(line2.begin() + center); };
 
-  step(points, 1);
-  step(points, -1);
-
-  // Greedy
-  /*
-  std::vector<IT> errors(points.size());
-  while (true) {
-    optimize(errors, points, 2, -2, 0.5);
-    if (current_size != points.size()) {
-      current_size = points.size();
-    } else {
-      break;
-    }
-  }
-  printf("done first step\n");
-
-  for (int i = 0; i <= 8; i++) {
-    printf("step %d\n", i);
-    optimize(errors, points, 2, -2, i / 8.0);
-  }
-  */
-
-
-  // Globally optimal
-  std::vector<std::vector<IT>> errors(9);
-  for (int i = 0; i < errors.size(); i++) {
-    errors[i].resize(points.size());
-    optimize(errors[i], points, 2, -2, i / (errors.size() - 1.0), -2);
-  }
-
-  printf("done first step\n");
-
-  int last_delete = -1;
-  std::vector<IT> best_error(errors.size());
-
-  while (points.size() > 4) {
-    for (int j = 0; j < errors.size(); j++) {
-      best_error[j] = optimize(errors[j], points, 2, -2,
-                               j / (errors.size() - 1.0), last_delete);
+    int margin = 1;
+    std::vector<double> errors;
+    errors.resize(points.size() - margin * 2);
+    for (int i = margin; i < points.size() - margin; i++) {
+      errors[i - margin] = error_remove(points, i, remove_op);
     }
 
-    IT smallest = best_error[0];
-    int in = 0;
-    for (int j = 1; j < best_error.size(); j++) {
-      if (best_error[j].t < smallest.t) {
-        smallest = best_error[j];
-        in = j;
-      }
-    }
+    int min;
+    float err = 0;
+    while (err < error) {
+      min = std::min_element(errors.begin(), errors.end()) - errors.begin();
+      err = errors[min];
 
-    printf("%f\n", smallest.t);
-    if (smallest.t < 1) {
-      Point new_point =
-          catmull_rom(points, smallest.i, in / (errors.size() - 1.0));
-      points.erase(points.begin() + smallest.i);
-      for (int j = 0; j < errors.size(); j++) {
-        errors[j].erase(errors[j].begin() + smallest.i);
-        for (int k = smallest.i; k < errors[j].size(); k++) {
-          errors[j][k].i -= 1;
+      if (err < error) {
+        fprintf(stderr, "Removing ");
+        errors.erase(errors.begin() + min);
+        points.erase(points.begin() + min + margin);
+        for (int i = min - 2; i < min + 2; i++) {
+          if (i >= 0 && i < errors.size()) {
+            errors[i] = error_remove(points, i + margin, remove_op);
+          }
         }
+      } else {
+        fprintf(stderr, "Stopping ");
       }
 
-      last_delete = smallest.i;
-      points[smallest.i] = new_point;
-    } else {
-      break;
+      fprintf(stderr, "%d %f %f %f\n", min + margin, err,
+              points[min + margin].x, points[min + margin].y);
     }
   }
 
-  if (points.size() != starting_size) {
-    printf("%d\n", (int)(points.size() - starting_size));
-  } else {
-    printf("0\n");
+  { // delete middle and shift surrounding points to the 1/3 and 2/3 positions
+    auto remove_op = [](std::vector<Point> &line1, std::vector<Point> &line2,
+                        int start, int center, int end) {
+      line2.erase(line2.begin() + center);
+      line2[center - 1] = catmull_rom(line1.data() + center - 1, 1.0 / 3);
+      line2[center] = catmull_rom(line1.data() + center, 2.0 / 3);
+    };
+
+    int margin = 2;
+    std::vector<double> errors;
+    errors.resize(points.size() - margin * 2);
+    for (int i = margin; i < points.size() - margin; i++) {
+      errors[i - margin] = error_remove(points, i, remove_op);
+    }
+
+    int min;
+    float err = 0;
+    while (err < error / 2) {
+      min = std::min_element(errors.begin(), errors.end()) - errors.begin();
+      err = errors[min];
+
+      if (err < error / 2) {
+        fprintf(stderr, "Removing ");
+        errors.erase(errors.begin() + min);
+        points.erase(points.begin() + min + margin);
+        for (int i = min - 2; i < min + 2; i++) {
+          if (i >= 0 && i < errors.size()) {
+            errors[i] = error_remove(points, i + margin, remove_op);
+          }
+        }
+      } else {
+        fprintf(stderr, "Stopping ");
+      }
+
+      fprintf(stderr, "%d %f %f %f\n", min + margin, err,
+              points[min + margin].x, points[min + margin].y);
+    }
   }
 
-  std::ofstream out("optimized_spline");
-  for (Point point : points) {
-    out << point.x << " " << point.y << "\n";
+  for (auto &p : points) {
+    printf("%f %f\n", p.x, p.y);
   }
 
   return 0;
---
+++ svg2jxl.py
@@ -0,0 +1,281 @@
+import argparse
+import math
+import random
+import subprocess
+import svgpathtools
+import sys
+import xml.etree.ElementTree
+from svgpathtools import CubicBezier, QuadraticBezier, Arc, Line
+
+# used for noise
+gradients = [
+  [0, 1],
+  [1, 0],
+  [0, -1],
+  [-1, 0],
+  [1, 1],
+  [1, -1],
+  [-1, 1],
+  [-1, -1],
+]
+
+
+def bezier1d(p0, p1, p2, p3, t):
+  return p0 * (1 - t)**3 + 3 * p1 * t * (1 - t)**2 + 3 * p2 * t**2 * (
+    1 - t) + p3 * t**3
+
+
+def bezier2d(p0, p1, p2, p3, t):
+  return (
+    bezier1d(p0[0], p1[0], p2[0], p3[0], t),
+    bezier1d(p0[1], p1[1], p2[1], p3[1], t),
+  )
+
+
+def bezier1d_dt(p0, p1, p2, p3, t):
+  return 3 * (1 - t)**2 * (p1 - p0) + 6 * t * (1 - t) * (
+    p2 - p1) + 3 * t**2 * (p3 - p2)
+
+
+def bezier2d_dt(p0, p1, p2, p3, t):
+  return (
+    bezier1d_dt(p0[0], p1[0], p2[0], p3[0], t),
+    bezier1d_dt(p0[1], p1[1], p2[1], p3[1], t),
+  )
+
+
+def sample_bezier(p0, p1, p2, p3):
+  points = []
+
+  t = 0
+  while t < 1:
+    if t > 1: break
+    points.append(bezier2d(p0, p1, p2, p3, t))
+    dt = bezier2d_dt(p0, p1, p2, p3, t)
+    l = length(dt[0], dt[1])
+    l = l or 10
+    t += 1 / l  # 1 is the absolute distance per step
+
+  if t > 1:
+    points.append(bezier2d(p0, p1, p2, p3, 1))
+
+  return points
+
+
+def diff(p1, p2):
+  return (p1[0] - p2[0], p1[1] - p2[1])
+
+
+def norm(p1):
+  length = math.sqrt(math.pow(p1[0], 2) + math.pow(p1[1], 2)) or 1
+  return (p1[0] / length, p1[1] / length)
+
+
+def length(x, y):
+  return math.sqrt(math.pow(x, 2) + math.pow(y, 2))
+
+
+def dist(p1, p2):
+  return length(p1[0] - p2[0], p1[1] - p2[1])
+
+
+def sim(p1, p2):
+  return round(p1[0]) == round(p2[0]) and round(p1[1]) == round(p2[1])
+
+
+def sample_line(p0, p1):
+  points = []
+  d = diff(p1, p0)
+  n = norm(d)
+  l = dist(p0, p1)
+  t = 0
+  while t < l:
+    points.append((p0[0] + t * n[0], p0[1] + t * n[1]))
+    t += 1
+  return points or [p0, p1]
+
+
+def optimize(points, error, scale):
+  points_s = "\n".join([f"{p[0]} {p[1]}" for p in points])
+  p = subprocess.run(["./optimize", f"{error * (scale ** 2)}"],
+                     input=points_s,
+                     text=True,
+                     universal_newlines=True,
+                     capture_output=True)
+  lines = [line.split(" ") for line in p.stdout.splitlines() if line]
+  return [(float(line[0]), float(line[1])) for line in lines]
+
+
+def create_spline(points, error, scale, color=[255, 255, 255]):
+  if len(points) > 2:
+    points = optimize(points, error, scale)
+
+  if len(points) < 2:
+    return ""
+
+  if len(points) == 2 and dist(points[0], points[1]) < 1:
+    # optionally add noise or ignore this case
+    return ""
+
+  points = [(round(p[0]), round(p[1])) for p in points]
+
+  for i in range(len(points) - 1):
+    if sim(points[i], points[i + 1]):
+      direction = random.choice(gradients)
+      points[i +
+             1] = [points[i][0] + direction[0], points[i][1] + direction[1]]
+
+  #new_color = [round(c / 255 * bitdepth * 2 - bitdepth, 2) for c in color]
+  new_color = [-7, -7, -7]
+  spline = [
+    "Spline",
+    f"{new_color[0]} 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0",
+    f"{new_color[1]} 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0",
+    f"{new_color[2]} 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0",
+    "0.7 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0"
+  ]
+
+  spline.extend([f"{round(p[0], 2)} {round(p[1], 2)}" for p in points])
+  spline.append("EndSpline")
+
+  return spline
+
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  parser.add_argument("input", help="Input SVG file")
+  parser.add_argument("output", help="Output jxl tree")
+  parser.add_argument("-s", "--scale", type=float, default=1)
+  parser.add_argument("-u", "--upsample", type=int, default=1)
+  parser.add_argument("-e",
+                      "--error",
+                      help="Allowed error, multiplied by scale^2",
+                      type=float,
+                      default=1)
+
+  args = parser.parse_args()
+
+  paths, attributes, svg_attribs = svgpathtools.svg2paths2(args.input)
+
+  tree = xml.etree.ElementTree.parse(args.input)
+  root = tree.getroot()
+
+  viewBox = [float(x) for x in root.attrib["viewBox"].split(" ")]
+
+  off_x = -viewBox[0]
+  off_y = -viewBox[1]
+
+  try:
+    width = float(root.attrib["width"])
+  except:
+    width = viewBox[2]
+
+  try:
+    height = float(root.attrib["height"])
+  except:
+    height = viewBox[3]
+
+  width *= args.scale
+  height *= args.scale
+
+  header = f"""Bitdepth 4
+  Upsample {args.upsample}
+  Width {round(width)}
+  Height {round(height)}
+  RCT 0
+  """
+
+  scale_x = width / viewBox[2]
+  scale_y = height / viewBox[3]
+
+  with open(args.output, "w+") as output:
+    output.write(header)
+    splines = []
+
+    for path, attribute in zip(paths, attributes):
+      current_spline = []
+      for segment in path:
+        if type(segment) in [CubicBezier, QuadraticBezier]:
+          p0 = (segment.start.real + off_x, segment.start.imag + off_y)
+          if type(segment) == QuadraticBezier:
+            p1 = (segment.control.real + off_x, segment.control.imag + off_y)
+            p2 = (segment.control.real + off_x, segment.control.imag + off_y)
+          else:
+            p1 = (segment.control1.real + off_x, segment.control1.imag + off_y)
+            p2 = (segment.control2.real + off_x, segment.control2.imag + off_y)
+          p3 = (segment.end.real + off_x, segment.end.imag + off_y)
+
+          p0 = (p0[0] * scale_x, p0[1] * scale_y)
+          p1 = (p1[0] * scale_x, p1[1] * scale_y)
+          p2 = (p2[0] * scale_x, p2[1] * scale_y)
+          p3 = (p3[0] * scale_x, p3[1] * scale_y)
+
+          cubic_points = sample_bezier(p0, p1, p2, p3)
+
+          if current_spline:
+            if dist(current_spline[0], cubic_points[0]) < 1:
+              current_spline.reverse()
+
+            if dist(current_spline[-1], cubic_points[-1]) < 1:
+              cubic_points.reverse()
+
+            if dist(current_spline[-1], cubic_points[0]) > 1:
+              spline = create_spline(current_spline, args.error, args.scale)
+              if spline:
+                spline_str = "\n".join(spline)
+                if spline_str not in splines:
+                  splines.append(spline_str)
+                  output.write(spline_str)
+                  output.write("\n")
+                  output.flush()
+              current_spline.clear()
+
+          if current_spline:
+            cubic_points.pop(0)
+
+          current_spline.extend(cubic_points)
+        elif type(segment) == Line:
+          p0 = (segment.start.real + off_x, segment.start.imag + off_y)
+          p1 = (segment.end.real + off_x, segment.end.imag + off_y)
+          p0 = (p0[0] * scale_x, p0[1] * scale_y)
+          p1 = (p1[0] * scale_x, p1[1] * scale_y)
+
+          cubic_points = sample_line(p0, p1)
+
+          if cubic_points:
+            if current_spline:
+              if dist(current_spline[0], cubic_points[0]) < 1:
+                current_spline.reverse()
+
+              if dist(current_spline[-1], cubic_points[-1]) < 1:
+                cubic_points.reverse()
+
+              if dist(current_spline[-1], cubic_points[0]) > 1:
+                spline = create_spline(current_spline, args.error, args.scale)
+                if spline:
+                  spline_str = "\n".join(spline)
+                  if spline_str not in splines:
+                    splines.append(spline_str)
+                    output.write(spline_str)
+                    output.write("\n")
+                    output.flush()
+                current_spline.clear()
+
+            if current_spline:
+              cubic_points.pop(0)
+
+            current_spline.extend(cubic_points)
+        else:
+          print(type(segment))
+
+      if current_spline:
+        spline = create_spline(current_spline, args.error, args.scale)
+        if spline:
+          spline_str = "\n".join(spline)
+          if spline_str not in splines:
+            splines.append(spline_str)
+            output.write(spline_str)
+            output.write("\n")
+            output.flush()
+
+    output.write("- Set 15")
