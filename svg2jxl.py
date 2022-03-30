ed64ede31b662d7008329997a1765fd8be6607e9
w <wvvwvvvvwvvw@gmail.com>
2022-03-30 04:05:24 -0700

    add typing and color

--- svg2jxl.py
+++ svg2jxl.py
@@ -1,12 +1,12 @@
 import argparse
 import math
 import random
-import subprocess
 import svgpathtools
 import sys
 import xml.etree.ElementTree
 from svgpathtools import CubicBezier, QuadraticBezier, Arc, Line
 import optimize
+from typing import Any
 
 # used for noise
 gradients = [
@@ -20,32 +20,34 @@ gradients = [
   [-1, -1],
 ]
 
+Point = tuple[float, float]
 
-def bezier1d(p0, p1, p2, p3, t):
+
+def bezier1d(p0: Point, p1: Point, p2: Point, p3: Point, t: float) -> Point:
   return p0 * (1 - t)**3 + 3 * p1 * t * (1 - t)**2 + 3 * p2 * t**2 * (
     1 - t) + p3 * t**3
 
 
-def bezier2d(p0, p1, p2, p3, t):
+def bezier2d(p0: Point, p1: Point, p2: Point, p3: Point, t: float) -> Point:
   return (
     bezier1d(p0[0], p1[0], p2[0], p3[0], t),
     bezier1d(p0[1], p1[1], p2[1], p3[1], t),
   )
 
 
-def bezier1d_dt(p0, p1, p2, p3, t):
+def bezier1d_dt(p0: Point, p1: Point, p2: Point, p3: Point, t: float) -> Point:
   return 3 * (1 - t)**2 * (p1 - p0) + 6 * t * (1 - t) * (
     p2 - p1) + 3 * t**2 * (p3 - p2)
 
 
-def bezier2d_dt(p0, p1, p2, p3, t):
+def bezier2d_dt(p0: Point, p1: Point, p2: Point, p3: Point, t: float) -> Point:
   return (
     bezier1d_dt(p0[0], p1[0], p2[0], p3[0], t),
     bezier1d_dt(p0[1], p1[1], p2[1], p3[1], t),
   )
 
 
-def sample_bezier(p0, p1, p2, p3):
+def sample_bezier(p0: Point, p1: Point, p2: Point, p3: Point) -> list[Point]:
   points = []
 
   t = 0
@@ -63,28 +65,30 @@ def sample_bezier(p0, p1, p2, p3):
   return points
 
 
-def diff(p1, p2):
+def diff(p1: Point, p2: Point) -> Point:
   return (p1[0] - p2[0], p1[1] - p2[1])
 
 
-def norm(p1):
-  length = math.sqrt(math.pow(p1[0], 2) + math.pow(p1[1], 2)) or 1
-  return (p1[0] / length, p1[1] / length)
-
-
-def length(x, y):
+def length(x: float, y: float) -> float:
   return math.sqrt(math.pow(x, 2) + math.pow(y, 2))
 
 
-def dist(p1, p2):
+def norm(p1: Point) -> Point:
+  p1_length = length(p1[0], p1[1]) or 1
+  return (p1[0] / p1_length, p1[1] / p1_length)
+
+
+# euclidean distance
+def dist(p1: Point, p2: Point) -> float:
   return length(p1[0] - p2[0], p1[1] - p2[1])
 
 
-def sim(p1, p2):
+# same rounded point
+def sim(p1: Point, p2: Point) -> bool:
   return round(p1[0]) == round(p2[0]) and round(p1[1]) == round(p2[1])
 
 
-def sample_line(p0, p1):
+def sample_line(p0: Point, p1: Point) -> list[Point]:
   points = []
   d = diff(p1, p0)
   n = norm(d)
@@ -95,10 +99,11 @@ def sample_line(p0, p1):
     t += 1
   return points or [p0, p1]
 
-def create_spline(points, error, scale):
+
+def create_spline(points: list[Point], args: Any, color: list[int]):
   if len(points) > 2:
     old_len = len(points)
-    points = optimize.optimize(points, error * (scale ** 2))
+    points = optimize.optimize(points, args.error * (args.scale**2))
     print(f"{old_len} -> {len(points)}")
 
   if len(points) < 2:
@@ -116,14 +121,23 @@ def create_spline(points, error, scale):
       points[i +
              1] = [points[i][0] + direction[0], points[i][1] + direction[1]]
 
-  #new_color = [round(c / 255 * bitdepth * 2 - bitdepth, 2) for c in color]
-  new_color = [-7, -7, -7]
+  if color[0] != color[1] or color[0] != color[2]:
+    thickness = args.thicknessc
+    sat = args.saturationc
+  else:
+    thickness = args.thickness
+    sat = args.saturation
+
+  new_color = [
+    round((c / 255 - 1) * (2**(args.bitdepth - 1) - 1)) * sat for c in color
+  ]
+
   spline = [
     "Spline",
     f"{new_color[0]} 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0",
     f"{new_color[1]} 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0",
     f"{new_color[2]} 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0",
-    "0.7 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0"
+    f"{thickness} 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0"
   ]
 
   spline.extend([f"{round(p[0], 2)} {round(p[1], 2)}" for p in points])
@@ -143,6 +157,27 @@ if __name__ == "__main__":
                       help="Allowed error, multiplied by scale^2",
                       type=float,
                       default=1)
+  parser.add_argument("-b", "--bitdepth", type=int, default=4)
+  parser.add_argument("-t",
+                      "--thickness",
+                      type=float,
+                      default=0.5,
+                      help="thickness for black lines")
+  parser.add_argument("-tc",
+                      "--thicknessc",
+                      type=float,
+                      default=0.5,
+                      help="thickness for colored lines")
+  parser.add_argument("-sat",
+                      "--saturation",
+                      type=float,
+                      default=1,
+                      help="saturation for black lines")
+  parser.add_argument("-satc",
+                      "--saturationc",
+                      type=float,
+                      default=1,
+                      help="saturation for colored lines")
 
   args = parser.parse_args()
 
@@ -169,7 +204,7 @@ if __name__ == "__main__":
   width *= args.scale
   height *= args.scale
 
-  header = f"""Bitdepth 4
+  header = f"""Bitdepth {args.bitdepth}
   Upsample {args.upsample}
   Width {round(width)}
   Height {round(height)}
@@ -184,6 +219,32 @@ if __name__ == "__main__":
     splines = []
 
     for path, attribute in zip(paths, attributes):
+      color = "#000000"
+      # Currently does not support classes (url(#id))
+
+      if "fill" in attribute:
+        color = attribute["fill"]
+        if color.startswith("url"):
+          continue
+
+      if "stroke" in attribute:
+        color = attribute["stroke"]
+
+      if color == "#FFFFFF": continue
+
+      color = color.lstrip("#")
+      if len(color) == 6:
+        color = [int(color[i:i + 2], 16) for i in (0, 2, 4)]
+      elif len(color) == 3:
+        color = [int(color[i:i + 1], 16) * 8 for i in (0, 1, 2)]
+      else:
+        color = [0, 0, 0]
+
+      if color[0] > 200 and color[1] > 200 and color[2] > 200: continue
+
+      if "fill-opacity" in attribute:
+        if float(attribute["fill-opacity"]) == 0: continue
+
       current_spline = []
       for segment in path:
         if type(segment) in [CubicBezier, QuadraticBezier]:
@@ -203,6 +264,9 @@ if __name__ == "__main__":
 
           cubic_points = sample_bezier(p0, p1, p2, p3)
 
+          if not cubic_points:
+            continue
+
           if current_spline:
             if dist(current_spline[0], cubic_points[0]) < 1:
               current_spline.reverse()
@@ -211,7 +275,7 @@ if __name__ == "__main__":
               cubic_points.reverse()
 
             if dist(current_spline[-1], cubic_points[0]) > 1:
-              spline = create_spline(current_spline, args.error, args.scale)
+              spline = create_spline(current_spline, args, color)
               if spline:
                 spline_str = "\n".join(spline)
                 if spline_str not in splines:
@@ -233,34 +297,36 @@ if __name__ == "__main__":
 
           cubic_points = sample_line(p0, p1)
 
-          if cubic_points:
-            if current_spline:
-              if dist(current_spline[0], cubic_points[0]) < 1:
-                current_spline.reverse()
+          if not cubic_points:
+            continue
 
-              if dist(current_spline[-1], cubic_points[-1]) < 1:
-                cubic_points.reverse()
+          if current_spline:
+            if dist(current_spline[0], cubic_points[0]) < 1:
+              current_spline.reverse()
 
-              if dist(current_spline[-1], cubic_points[0]) > 1:
-                spline = create_spline(current_spline, args.error, args.scale)
-                if spline:
-                  spline_str = "\n".join(spline)
-                  if spline_str not in splines:
-                    splines.append(spline_str)
-                    output.write(spline_str)
-                    output.write("\n")
-                    output.flush()
-                current_spline.clear()
+            if dist(current_spline[-1], cubic_points[-1]) < 1:
+              cubic_points.reverse()
 
-            if current_spline:
-              cubic_points.pop(0)
+            if dist(current_spline[-1], cubic_points[0]) > 1:
+              spline = create_spline(current_spline, args, color)
+              if spline:
+                spline_str = "\n".join(spline)
+                if spline_str not in splines:
+                  splines.append(spline_str)
+                  output.write(spline_str)
+                  output.write("\n")
+                  output.flush()
+              current_spline.clear()
 
-            current_spline.extend(cubic_points)
+          if current_spline:
+            cubic_points.pop(0)
+
+          current_spline.extend(cubic_points)
         else:
           print(type(segment))
 
       if current_spline:
-        spline = create_spline(current_spline, args.error, args.scale)
+        spline = create_spline(current_spline, args, color)
         if spline:
           spline_str = "\n".join(spline)
           if spline_str not in splines:
@@ -269,4 +335,4 @@ if __name__ == "__main__":
             output.write("\n")
             output.flush()
 
-    output.write("- Set 15")
+    output.write(f"- Set {2 ** args.bitdepth - 1}")
